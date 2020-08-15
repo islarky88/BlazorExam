5ef01db70734d4da05f7e0c0f14255d4f7137453
islarky88 <islarky88@gmail.com>
2020-08-15 14:53:43 +0800

    testing passing of data to child components

--- wwwroot/scripts/main.js
+++ wwwroot/scripts/main.js
@@ -1,6 +1,18 @@
 window['ShowAlert'] = (message) => {
     alert('asdasd' + message);
 };
+window['GetHeaders'] = (message) => {
+    alert('GetHeaders: ' + message);
+    return "GetHeaders Message: " + message;
+};
+window['FetchSavedItems'] = (type) => {
+    let data = [];
+    // cgeck if there is already and entry
+    if (localStorage.getItem(type)) {
+        data = JSON.parse(localStorage.getItem(type));
+    }
+    return JSON.stringify(data);
+};
 window['SaveToLocalStorage'] = (type, message) => {
     let data = [];
     // cgeck if there is already and entry
--- wwwroot/scripts/main.ts
+++ wwwroot/scripts/main.ts
@@ -2,6 +2,23 @@ window['ShowAlert'] = (message) => {
   alert('asdasd' + message);
 }
 
+window['GetHeaders'] = (message) => {
+  alert('GetHeaders: ' + message);
+  return "GetHeaders Message: " + message;
+}
+
+
+window['FetchSavedItems'] = (type) => {
+
+  let data: JsonData[] = [];
+
+  // cgeck if there is already and entry
+  if (localStorage.getItem(type)) {
+    data = JSON.parse(localStorage.getItem(type));
+  }
+  return JSON.stringify(data)
+}
+
 window['SaveToLocalStorage'] = (type: string, message: JsonData) => {
 
   let data: JsonData[] = [];
